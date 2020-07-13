27fd133e2e8ff31a9829494f2987ed7185a065e1
Gilbert Nordelus <havegil@gmail.com>
2020-07-13 10:09:33 -0400

    Layout bug list fixes
    Signed-off-by: Gilbert Nordelus <havegil@gmail.com>

--- js/work-flow-nav.js
+++ js/work-flow-nav.js
@@ -6,25 +6,37 @@ $(document).ready(function(){
 
 
         $('.work-flow_menu ul li.active').removeClass('active');
-        $(this).addClass("active");
-
-        if($('#home-menu').length > 0){
-            return;
-        }
-
+        $(this).addClass("active");       
+            
         if (!($( ".work-flow_code" ).is(":visible")) ) {
             
             var $el = $(this).children();
             $($el).toggleClass( 'open');
+            
 
         }else{
 
 
            $('div.work-flow-menu-support.work-flow-support-mode-mobile.open ').removeClass('open');
 
-            var children = $(this).children().clone(true,true);
-            var $el = $(children[1]).removeClass('work-flow-support-mode-mobile');
-            $("#sdk-support").html($el);
+           //remove recently added info copy and button
+           $( "#sdk-support .work-flow-menu-support" ).remove();
+
+           //now cloned and prepend new selected info copy and button
+           var clonedEl = $(this).children().clone(true,true);
+           var $currentEl = $(clonedEl[1]).removeClass('work-flow-support-mode-mobile');
+           var itemID =  $(this).attr('id') + "-code";
+
+           //with old copy and button removed, prepend newly clone copy and button, make sure we're not at the home menu
+           if($('#home-menu').length <= 0){
+            $($currentEl).prependTo("#sdk-support");
+           }
+
+           //remove the open class on all children and mavailable the one selected
+           var $el = $('.code-viewer-wrapper').children();
+           $($el).removeClass('code-window-opened');
+           $(".code-viewer-wrapper #" + itemID + ".sdk-embed-wrapper").addClass('code-window-opened');
+          
         }
 
     });
@@ -50,8 +62,8 @@ $(document).ready(function(){
 
 //Function to the css rule
 function resetWorkFlowMenu(){
-    console.log("deactivating work flow menu if active")
+   // console.log("deactivating work flow menu if active")
     $('div.work-flow-support-mode-mobile.open').removeClass('open');
     $('div.work-flow-menu-item.active').removeClass('active');
-    $('#sdk-support').html('');
+   // $('#sdk-support').html('');
 }
