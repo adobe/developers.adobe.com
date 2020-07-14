1a2e796d003b8ff720e490b54575427546199a06
Gilbert Nordelus <havegil@gmail.com>
2020-07-14 09:03:59 -0400

    Pushing all fixes from review rounds

--- js/work-flow-nav.js
+++ js/work-flow-nav.js
@@ -4,15 +4,13 @@ $(document).ready(function(){
 
     $('.work-flow_menu ul li').on( "click", function(e) {
 
-
         $('.work-flow_menu ul li.active').removeClass('active');
         $(this).addClass("active");       
             
         if (!($( ".work-flow_code" ).is(":visible")) ) {
             
             var $el = $(this).children();
-            $($el).toggleClass( 'open');
-            
+            $($el).toggleClass( 'open');            
 
         }else{
 
@@ -35,10 +33,8 @@ $(document).ready(function(){
            //remove the open class on all children and mavailable the one selected
            var $el = $('.code-viewer-wrapper').children();
            $($el).removeClass('code-window-opened');
-           $(".code-viewer-wrapper #" + itemID + ".sdk-embed-wrapper").addClass('code-window-opened');
-          
+           $(".code-viewer-wrapper #" + itemID + ".sdk-embed-wrapper").addClass('code-window-opened');          
         }
-
     });
 
 
@@ -52,9 +48,52 @@ $(document).ready(function(){
     })
 
 
+    
+ //Prism copy to clipboard
+    //
+    // al pre tags on the page
+    const pres = document.getElementsByTagName("pre")
 
+    //
+    // reformat html of pre tags
+    if (pres !== null ) {
+        for (let i = 0; i < pres.length; i++) {
+            // check if its a pre tag with a prism class
+            if (isPrismClass(pres[i])) {
+                // insert code and copy element
+                pres[i].innerHTML = `<div class="copy">copy</div><code class="${pres[i].className}">${pres[i].innerHTML}</code>` 
+                console.log(pres[i].innerHTML)     
+            }
+        }
+    }
+
+
+    // create clipboard for every copy element
+    const clipboardJS = new clipboardJS('.copy', {
+        target: (trigger) => {
+            console.log("I made it here");
+            return trigger.nextElementSibling;
+        }
+    });
+
+    //
+    // do stuff when copy is clicked
+    clipboardJS.on('success', (event) => {
+    event.trigger.textContent = 'copied!';
+    setTimeout(() => {
+        event.clearSelection();
+        event.trigger.textContent = 'copy';
+    }, 2000);
+    });
+
+    //
+    // helper function
+    function isPrismClass(preTag) { 
+        return preTag.className.substring(0, 8) === 'language'
+    }
+
+   
     //window resize to properly deal with the animation on mobile TBD
-
     // run test on resize of the window
     $(window).resize(resetWorkFlowMenu);
 
@@ -65,5 +104,5 @@ function resetWorkFlowMenu(){
    // console.log("deactivating work flow menu if active")
     $('div.work-flow-support-mode-mobile.open').removeClass('open');
     $('div.work-flow-menu-item.active').removeClass('active');
-   // $('#sdk-support').html('');
 }
+
