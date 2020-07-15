724edb07327b483cbcef95252d064aa087caed8d
Simon Wex <simon@simonwex.com>
2020-07-14 21:46:27 -0700

    Removing prism plugin

--- _plugins/jekyll-prism-plugin.rb
+++ _plugins/jekyll-prism-plugin.rb
@@ -2,9 +2,9 @@ module Jekyll
 
     class PrismBlock < Liquid::Block
       include Liquid::StandardFilters
-  
+
       OPTIONS_SYNTAX = %r{^([a-zA-Z0-9.+#-]+)((\s+\w+(=[0-9,-]+)?)*)$}
-  
+
       def initialize(tag_name, markup, tokens)
         super
         if markup.strip =~ OPTIONS_SYNTAX
@@ -26,14 +26,14 @@ module Jekyll
           raise SyntaxError.new("Syntax Error in 'prism' - Valid syntax: prism <lang> [linenos(='1-5')]")
         end
       end
-  
+
       def render(context)
         code = h(super).strip
-  
+
         if @options["linenos"] == true
           @options["linenos"] = "1-#{code.lines.count}"
         end
-  
+
         <<-HTML
   <div>
     <pre data-line='#{@options["linenos"]}'><code class='language-#{@lang}'>#{code}</code></pre>
@@ -41,7 +41,4 @@ module Jekyll
         HTML
       end
     end
-  
   end
-  
-  Liquid::Template.register_tag('prism', Jekyll::PrismBlock)
